38b14ec56fa0aac36db92a416ef1fb5aa5a61564
Lac0sta <adv.a.frolov@gmail.com>
2025-04-06 08:51:41 +0200

    first commit, gitignore

---
+++ Movie_quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie_quiz
+//
+//  Created by Aleksei Frolov on 06.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
